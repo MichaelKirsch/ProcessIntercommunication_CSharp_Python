813fb8afa100ea4215f56f0b953dfa7c4b1f290d
Michael <michael_kirsch@t-online.de>
2021-04-18 13:37:36 +0200

    Initial commit

---
+++ PipesTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new process object
+            Process myProcess = new Process();
+
+            //Provide the start information for the process
+            myProcess.StartInfo.FileName = "python.exe";
+            myProcess.StartInfo.Arguments = "pipeback.py framerate=60";
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.RedirectStandardInput = true;
+            myProcess.StartInfo.RedirectStandardOutput = true;
+
+            StreamReader myStreamReader;
+            StreamWriter myStreamWriter;
+
+            //Invoke the process from current process
+            myProcess.Start();
+
+            myStreamReader = myProcess.StandardOutput;
+            myStreamWriter = myProcess.StandardInput;
+
+            int TESTFREQUENCY = 100;
+            
+            while (true)
+            {
+                
+                
+                String argument = "GIMME MESSAGE BIATCH";
+                myStreamWriter.WriteLine(argument);
+                //Read the standard output of the spawned process.
+                string myString = myStreamReader.ReadLine();
+                Console.WriteLine(myString);
+                Thread.Sleep(1000/TESTFREQUENCY);
+            }
+            
+            myProcess.WaitForExit();
+            myStreamWriter.Close();
+            myStreamReader.Close();
+            myProcess.Close();
+        }
+    }
+}
---
+++ PipesTest/pipeback.py
@@ -0,0 +1,33 @@
+﻿import sys
+import getpass
+import time
+
+
+
+
+def main(args,**kwargs):
+    reqiredarg = ""
+    TESTFREQUENCY_HZ = 60
+    close = False
+    counter = 0
+    # Define for Types: 0=Int, 1=Float, 3 = Char, 4 = Double, 5 = Long
+    
+    # Message Format = Name,Type,Data$ 
+    #Example "TestVariable,0,10$Test2Variable,1,10.23123$"
+    
+
+    for key in args[1:]:
+        arg,value = key.split("=")
+        if arg == "framerate":
+            TESTFREQUENCY_HZ = int(value)
+    
+    while not close:
+        sys.stdout.flush()
+        counter+=1
+        recaived_message = sys.stdin.readline().rstrip()
+        if recaived_message == "GIMME MESSAGE BIATCH":
+            print("Received: "+recaived_message+", Here are the Test Values:",counter)
+        time.sleep(1.0 / TESTFREQUENCY_HZ)
+    
+if __name__ == "__main__":
+    main(sys.argv)
